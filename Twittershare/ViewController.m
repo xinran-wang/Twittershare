a08c6774024ddcc2d9f70aad5f142389c61b2699
xinran-wang <u586@intrafile.com>
2018-05-20 10:08:58 -0700

    Add twitter controller

--- Twittershare/ViewController.m
+++ Twittershare/ViewController.m
@@ -7,12 +7,25 @@
 //
 
 #import "ViewController.h"
+#import "Social/Social.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextView *twittTextView;
 
 @end
 
 @implementation ViewController
+
+
+- (void) showAlertMessage:(NSString*) msg {
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"" message:msg preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleCancel handler:nil];
+    
+    [alertController addAction:cancelAction];
+    
+    [self presentViewController:alertController animated:YES completion:nil];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -23,6 +36,31 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)showShare:(id)sender {
+    if ([self.twittTextView isFirstResponder]) {
+        [self.twittTextView resignFirstResponder];
+    }
+    
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Test Title" message:@"hello Xinran" preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil];
+    
+    UIAlertAction *twittAction = [UIAlertAction actionWithTitle:@"Tweet" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        if ([SLComposeViewController isAvailableForServiceType:SLServiceTypeTwitter]) {
+            SLComposeViewController *twitterController = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
+            [twitterController setInitialText:[self.twittTextView.text substringToIndex:140]];
+            [self presentViewController:twitterController animated:YES completion:nil];
+        } else {
+            [self showAlertMessage:@"Please signin twitter"];
+        }
+    }];
+    
+    [alertController addAction:cancelAction];
+    
+    [alertController addAction:twittAction];
+    
+    [self presentViewController:alertController animated:YES completion:nil];
 }
 
 
